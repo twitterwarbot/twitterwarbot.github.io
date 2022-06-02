73882cebf9b20141ef7250b0732ef410865bff2c
Marcos <m.castrillosarmiento@gamefound.com>
2022-06-02 13:14:32 +0100

    Disable forms

--- src/main.js
+++ src/main.js
@@ -3,24 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Vue from "vue";
 
-import TwitchForm from "./components/TwitchForm.vue";
-import ParamoForm from "./components/ParamoForm.vue";
+// import TwitchForm from "./components/TwitchForm.vue";
+// import ParamoForm from "./components/ParamoForm.vue";
 import ThankYou from "./components/ThankYou.vue";
-import Participants from "./components/Participants.vue";
-import Injures from "./components/Injures.vue";
-import Powerups from "./components/Powerups.vue";
-import Weapons from "./components/Weapons.vue";
+// import Participants from "./components/Participants.vue";
+// import Injures from "./components/Injures.vue";
+// import Powerups from "./components/Powerups.vue";
+// import Weapons from "./components/Weapons.vue";
 
 Vue.config.productionTip = false;
 
 const routes = {
   "/thanks": ThankYou,
-  "/twitch": TwitchForm,
-  "/paramo": ParamoForm,
-  "/participants": Participants,
-  "/injures": Injures,
-  "/powerups": Powerups,
-  "/weapons": Weapons,
+  "/twitch": ThankYou,
+  "/paramo": ThankYou,
+  "/participants": ThankYou,
+  "/injures": ThankYou,
+  "/powerups": ThankYou,
+  "/weapons": ThankYou,
 };
 
 new Vue({
