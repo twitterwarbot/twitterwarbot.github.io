24008a329b303c288e5a4e17be4d65d99ca92a62
sokram <sokramcs@pm.me>
2024-08-12 21:43:54 +0200

    close form

--- src/main.js
+++ src/main.js
@@ -4,23 +4,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Vue from "vue";
 
 // import TwitchForm from "./components/TwitchForm.vue";
-import ParamoForm from "./components/ParamoForm.vue";
+// import ParamoForm from "./components/ParamoForm.vue";
 import ThankYou from "./components/ThankYou.vue";
-import Participants from "./components/Participants.vue";
-import Injures from "./components/Injures.vue";
-import Powerups from "./components/Powerups.vue";
-import Weapons from "./components/Weapons.vue";
+// import Participants from "./components/Participants.vue";
+// import Injures from "./components/Injures.vue";
+// import Powerups from "./components/Powerups.vue";
+// import Weapons from "./components/Weapons.vue";
 
 Vue.config.productionTip = false;
 
 const routes = {
   "/thanks": ThankYou,
   // "/twitch": ThankYou,
-  "/paramo": ParamoForm,
-  "/participants": Participants,
-  "/injures": Injures,
-  "/powerups": Powerups,
-  "/weapons": Weapons,
+  // "/paramo": ParamoForm,
+  // "/participants": Participants,
+  // "/injures": Injures,
+  // "/powerups": Powerups,
+  // "/weapons": Weapons,
 };
 
 new Vue({
